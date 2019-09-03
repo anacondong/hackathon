03d4000f47aa93bfcb7c07edcfc94f0363f6a7b1
ittipol jannapast <ittipol.jannapast@nbc.ca>
2019-09-03 10:35:57 -0400

    Move screen to component
    Move screen to component

---
+++ screens/linksScreen/index.js
@@ -0,0 +1,18 @@
+import { connect } from 'react-redux';
+import LinksScreen from './LinksScreen';
+import * as  counterActions from '../../src/actions/counterActions'
+
+const mapStateToProps = state => {
+    return {
+        // counter: state.counterReducer.counter,
+    }
+  }
+
+const mapDispatchToProps = dispatch => {
+    return {
+        // increaseCounter: () => dispatch(counterActions.increaseCounter()),
+        // decreaseCounter: () => dispatch(counterActions.decreaseCounter()),
+    }
+  }
+  
+  export default connect(mapStateToProps, mapDispatchToProps)(LinksScreen);
---
+++ screens/settingsScreen/css.js
@@ -0,0 +1,14 @@
+import {
+    StyleSheet,
+    Platform,
+  } from 'react-native';
+  
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+    },
+  });
+
+  export default styles;
